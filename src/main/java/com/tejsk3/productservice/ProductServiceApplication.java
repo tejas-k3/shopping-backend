69a4ca44ae329db71a78d84d9451727c8550ba30
Tejas <tejaskothari03@gmail.com>
2023-05-27 19:23:00 +0530

    Commented db start stop cmds

--- src/main/java/com/tejsk3/productservice/ProductServiceApplication.java
+++ src/main/java/com/tejsk3/productservice/ProductServiceApplication.java
@@ -5,9 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
+	/*
+	First need to make sure MongoDB instance is running
+	C:\Users\TEJAS>net start MongoDB
+	The MongoDB Server (MongoDB) service is starting.
+	The MongoDB Server (MongoDB) service was started successfully.
 
+	And stopping it once work is done!
+	C:\Users\TEJAS>net stop MongoDB
+	The MongoDB Server (MongoDB) service is stopping.
+	The MongoDB Server (MongoDB) service was stopped successfully.
+	 */
 }
